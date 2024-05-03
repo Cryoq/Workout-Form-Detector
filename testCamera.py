1e1ec55f0d04d2e7354b0b3ba528a1fc6f8f8100
Cryoq <99939677+Cryoq@users.noreply.github.com>
2024-05-03 08:56:57 -0500

    Update testCamera.py

--- testCamera.py
+++ testCamera.py
@@ -4,7 +4,6 @@ import numpy as np
 import pygame_menu.themes
 from tracking.curl import workout
 import cv2
-from statistics import mean
 
 def calculate_euclidean_distance(keypoints1, keypoints2):
     # Calculate Euclidean distance between two sets of keypoints
@@ -24,7 +23,7 @@ def calculate_distance(realtime_keypoints, dataset):
             min_distance = distance
             closest_point = data_point
 
-    return min_distance, closest_point
+    return min_distance
 
 screen_width = 800
 screen_height = 600
@@ -42,84 +41,34 @@ def open_camera_screen(workout) -> None:
     workout.setupCamera()  # Initialize camera capture
     leftCurlPoints = np.load("data/leftCurlPoints.npy")
     
-    angles = []
-    buffer = 0
-
-    distanceList = []
-    goodForm = True
-    okForm = False
-    badForm = False
-
-    maxGoodForm = 0.09
-    maxOkForm = 0.11
-
-
-    running = True
-    curlUp = False
-
-    rep = 0
-    
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
+        # runs one frame of the workout 
         frame = workout.oneFrame()
+        
         if frame is not None:
+            # Converts cv2 frame to work with pygame and blits it onto the screen
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  
-            frame = np.rot90(frame)  # Rotate frame 90 degrees clockwise
-            frame = pygame.surfarray.make_surface(frame)  # Convert frame to pygame surface
-            surface.blit(frame, (screen_width/8, screen_height/8))  # Blit frame onto pygame surface
-            pygame.display.flip()  # Update display
+            frame = np.rot90(frame)
+            frame = pygame.surfarray.make_surface(frame)
+            surface.blit(frame, (screen_width/8, screen_height/8))
+            pygame.display.flip()
             
-        shoulder, wrist, elbow = workout.returnPoints()
-        realtime_keypoints = np.array([wrist, elbow, shoulder])
-        distance, closest_point = calculate_distance(realtime_keypoints, leftCurlPoints)
+        # Calculates distance between good form dataset and real time form data
+        Lwrist, Lelbow, Lshoulder = workout.returnPoints(True, False)
+        realtime_keypoints = np.array([Lwrist, Lelbow, Lshoulder])
+        distance = calculate_distance(realtime_keypoints, leftCurlPoints)
         print("Distance", distance)
         
-        angle = workout.curl()
+        # The main curl workout part
+        form, rep = workout.curl(distance, Lwrist, Lelbow, Lshoulder)
         
-        if buffer < 3:
-            angles.append(angle)
-            distanceList.append(distance)
-            buffer += 1
-        if buffer >= 3:
-            if mean(angles) > angles[0]:
-                print("Down")
-            else:
-                print("Up")
-            if mean(distanceList) < maxGoodForm:
-                goodForm = True
-                okForm = False
-                badForm = False
-            elif mean(distanceList) < maxOkForm:
-                goodForm = False
-                okForm = True
-                badForm = False
-            else:
-                goodForm = False
-                okForm = False
-                badForm = True
-                
-            if goodForm:
-                print("You have great form")
-            elif okForm:
-                print("Your form is ok")
-            elif badForm:
-                print("Your form is terrible")
-            
-            angles = []
-            distanceList = []
-            buffer = 0
-
-        if angle <= 35.0 and not curlUp:
-            rep += 1
-            curlUp = True
-        elif angle >= 160 and curlUp:
-            curlUp = False
-
-        print(f"Your on rep: {rep}")
+        print(form)
+        print(f"You are on rep {rep}")
 
     workout.releaseCamera()
 
